d62d11d5ca21d5ab14826a9a6988b64f64faf374
Pan <68371938+Eyepan@users.noreply.github.com>
2023-04-05 22:09:02 +0530

    ready for aws yay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from database import DB
 
 
@@ -15,6 +15,32 @@ def index():
 def show_students():
     students = my_db.run_query("SELECT * FROM students")
     return render_template("students.html", students=students)
+
+
+@app.route("/students", methods=['POST'])
+def add_student():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    mail = request.form.get("mail")
+    phone = request.form.get("phone")
+    dob = request.form.get("dob")
+    gender = request.form.get("gender")
+    address_1 = request.form.get("address_1")
+    address_2 = request.form.get("address_2")
+    city = request.form.get("city")
+    state = request.form.get("state")
+    zip = request.form.get("zip")
+    country = request.form.get("country")
+
+    l = len(my_db.run_query("SELECT * FROM students"))
+    my_db.run_query("INSERT INTO students VALUES (:id, :f, :l, :m, :p, :d, :g, :a1, :a2, :c, :s, :z, :co)",
+                    id=l, f=first_name, l=last_name, m=mail, p=phone, d=dob, g=gender, a1=address_1, a2=address_2, c=city, s=state, z=zip, co=country)
+    return redirect("/students")
+
+
+@app.route("/create")
+def create():
+    return render_template("addstudent.html")
 
 
 if __name__ == "__main__":
